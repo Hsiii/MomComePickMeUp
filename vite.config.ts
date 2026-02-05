ca3e8793e4aaf1c93b937f006bd5f8a798f9a2a9
陳曦 <eriksonchen@gmail.com>
2026-02-05 23:17:33 +0800

    chore: add react compiler

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,18 @@ import react from '@vitejs/plugin-react';
 import { defineConfig } from 'vite';
 import { VitePWA } from 'vite-plugin-pwa';
 
+const ReactCompilerConfig = {
+    /* React Compiler configuration */
+};
+
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [
-        react(),
+        react({
+            babel: {
+                plugins: [['babel-plugin-react-compiler', ReactCompilerConfig]],
+            },
+        }),
         VitePWA({
             registerType: 'autoUpdate',
             includeAssets: [
